1d0b0b9b7ba17a95c3dec8aef71b606e19a4239e
theplangel <theplangel@gamil.com>
2026-05-12 07:47:56 -0600

    test(frontend): add index.html and login E2E test for Cypress

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -4,6 +4,7 @@ module.exports = defineConfig({
   e2e: {
     specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
     supportFile: false,
+    baseUrl: 'http://localhost:8080'
   },
   video: false,
 })
--- frontend/cypress/e2e/spec.cy.js
+++ frontend/cypress/e2e/spec.cy.js
@@ -1,6 +1,9 @@
-describe('Homepage', () => {
-  it('loads the static index page', () => {
+describe('Login flow', () => {
+  it('allows a user to login with valid credentials', () => {
     cy.visit('/')
-    cy.contains('crud-empleados frontend').should('exist')
+    cy.get('#username').type('user')
+    cy.get('#password').type('pass')
+    cy.get('#login-button').click()
+    cy.contains('Welcome, user').should('exist')
   })
 })
